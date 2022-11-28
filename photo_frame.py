02f3d0b66811dc443b0725b3faa0e43b36c9efaf
Morgan <redfieldm@gmail.com>
2022-11-28 12:27:20 -0500

    convert to class

--- photo_frame.py
+++ photo_frame.py
@@ -6,66 +6,83 @@ import tkinter
 from PIL import Image, ImageTk
 from signal import signal, SIGINT
 
-def get_random_img(dir_path):
-    """
-    Randomly select an image filename within `dir_path` and all subdirs
-    """
-    # randomly get a file
-    # /media/photos/409.jpeg
 
-    img_files = []
+class PhotoFrame:
+    def __init__(self, imgpath):
+        self.dir_path = imgpath
 
-    # see also
-    # https://stackoverflow.com/questions/2909975/python-list-directory-subdirectory-and-files
-    for path, subdirs, files in os.walk(dir_path):
-        for name in files:
-            img_files.append(os.path.join(path, name))
+        self.root = tkinter.Tk()
+        self.w, self.h = self.root.winfo_screenwidth(), root.winfo_screenheight()
+        self.root.overrideredirect(1)
+        self.root.geometry("%dx%d+0+0" % (self.w, self.h))
+        self.root.focus_set()    
+        self.root.bind("<Escape>", lambda e: (e.widget.withdraw(), e.widget.quit()))
+        self.canvas = tkinter.Canvas(root,width=self.w,height=self.h)
+        self.canvas.pack()
+        self.canvas.configure(background='black')
 
-    return random.choice(img_files)
+    def get_random_img(self):
+        """
+        Randomly select an image filename within `dir_path` and all subdirs
+        """
+        # randomly get a file
+        # /media/photos/409.jpeg
 
-def display_img(img_fname, canvas):
-    """
-    display an image stored at img_fname, taking up the whole canvas
-    """
-    try:
-        # open the image
-        im = Image.open(img_fname)
+        img_files = []
 
-        # display image
-        # mostly thanks to
-        # https://stackoverflow.com/questions/47316266/can-i-display-image-in-full-screen-mode-with-pil
-        
+        # see also
+        # https://stackoverflow.com/questions/2909975/python-list-directory-subdirectory-and-files
+        for path, subdirs, files in os.walk(self.dir_path):
+            for name in files:
+                img_files.append(os.path.join(path, name))
 
-        w = int(canvas.cget("width"))
-        h = int(canvas.cget("height"))
-        imgWidth, imgHeight = im.size
-        if imgWidth > w or imgHeight > h:
-            ratio = min(w/imgWidth, h/imgHeight)
-            imgWidth = int(imgWidth*ratio)
-            imgHeight = int(imgHeight*ratio)
-            im = im.resize((imgWidth,imgHeight), Image.ANTIALIAS)
-        image = ImageTk.PhotoImage(im)
-        imagesprite = canvas.create_image(w/2,h/2,image=image)
+        self.img_fname = random.choice(img_files)
 
-        return True
-    except:
-        return False
+    def display_img(self):
+        """
+        display an image stored at img_fname, taking up the whole canvas
+        """
+        try:
+            # open the image
+            im = Image.open(self.img_fname)
 
-def slideshow(path, canvas):
-    """
-    Run a slideshow of random images from the specified location
-    """
-    # get and display img
-    imgname = get_random_img(path)
-    if (display_img(imgname, canvas)):
-        # TODO: buttons for "don't show" and "like"
-        # wait for a random amount of time (2 to 5 minutes per image)
-        delay_time = random.randrange(2*60, 5*60)*1000
-    else:
-        # if we didn't successfully display an image, delay should be 0
-        print("not a good image: " + imgname)
-        delay_time = 1 # super short
-    canvas.after(delay_time, slideshow, path, canvas)
+            # display image
+            # mostly thanks to
+            # https://stackoverflow.com/questions/47316266/can-i-display-image-in-full-screen-mode-with-pil
+            
+            imgWidth, imgHeight = im.size
+            if imgWidth > self.w or imgHeight > self.h:
+                ratio = min(self.w/imgWidth, self.h/imgHeight)
+                imgWidth = int(imgWidth*ratio)
+                imgHeight = int(imgHeight*ratio)
+                im = im.resize((imgWidth,imgHeight), Image.ANTIALIAS)
+            image = ImageTk.PhotoImage(im)
+            imagesprite = self.canvas.create_image(self.w/2,self.h/2,image=image)
+
+            return True
+        except:
+            return False
+
+    def slideshow(self):
+        """
+        Run a slideshow of random images from the specified location
+        """
+        # get and display img
+        self.get_random_img()
+        print("Attempting to show: " + self.imgname)
+        if (self.display_img()):
+            # TODO: buttons for "don't show" and "like"
+            # wait for a random amount of time (2 to 5 minutes per image)
+            delay_time = random.randrange(2*60, 5*60)*1000
+        else:
+            # if we didn't successfully display an image, delay should be 0
+            print("not a good image: " + self.imgname)
+            delay_time = 1 # super short
+        self.root.after(delay_time, self.slideshow)
+
+    def run_show(self):
+        self.slideshow()
+        self.root.mainloop()
 
 def show_usage(scriptname):
     print(scriptname + " -p path/to/imgs")
@@ -95,18 +112,8 @@ if __name__ == "__main__":
 
     print("looking for images in: " + imgpath)
 
-    root = tkinter.Tk()
-    w, h = root.winfo_screenwidth(), root.winfo_screenheight()
-    root.overrideredirect(1)
-    root.geometry("%dx%d+0+0" % (w, h))
-    root.focus_set()    
-    root.bind("<Escape>", lambda e: (e.widget.withdraw(), e.widget.quit()))
-    canvas = tkinter.Canvas(root,width=w,height=h)
-    canvas.pack()
-    canvas.configure(background='black')
-
-    slideshow(imgpath, canvas)
+    pf = PhotoFrame(imgpath)
 
     signal(SIGINT, handler)
 
-    root.mainloop()
+    pf.run_show()
